8c24b67e67257a0e4b4d25736d887d66a49a6bc0
Jose Veliz <josecarlosvelizcastro@gmail.com>
2021-09-30 00:45:30 -0600

    Servicios agregados

--- frontend_weatherapp/src/app/src/app/components/login/login.component.ts
+++ frontend_weatherapp/src/app/src/app/components/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { User } from "../../models/user";
+
+import { UserServiceService } from "../../services/user-service/user-service.service";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +14,12 @@ export class LoginComponent implements OnInit {
   public email: string = "";
   public password: string = "";
 
-  constructor(private router: Router) { }
+  public user: User = {
+    username: "",
+    description: ""
+  }
+
+  constructor(private router: Router, private userService: UserServiceService) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +27,16 @@ export class LoginComponent implements OnInit {
   login(){
     console.log(this.email);
     console.log(this.password);
+    this.user.username = this.email;
+    this.user.description = this.password;
+    this.userService.getUsers()
+    .subscribe(
+      res => {
+        console.log(res);
+      }, error => {
+        console.log(error);
+      }
+    );
     this.router.navigate(['/profile'])
   }
 }
--- frontend_weatherapp/src/app/src/app/services/user-service/user-service.service.ts
+++ frontend_weatherapp/src/app/src/app/services/user-service/user-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from "../../../../../environments/environment";
+import { User } from "../../models/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  insertUser(user: User){
+    return this.http.post(`${environment.apiUrl}users`, user);
+  }
+
+  getUsers(){
+    return this.http.get(`${environment.apiUrl}users`)
+  }
 }
